0ef90ca1a799dae34f2d547c5e063e6c2acf061b
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2021-06-19 11:14:12 +0300

    Add files via upload

---
+++ Coronavirus Web Scaper/Enums/NameByEkatte.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coronavirus_Web_Scaper.Models
+{	
+	/// <summary>
+	/// This is a enumeration that has in it all names of the regions that are in Bulgaria
+	/// </summary>
+	public enum NameByEkatte
+	{
+		BLG,
+		BGS,
+		VAR,
+		VTR,
+		VID,
+		VRC,
+		GAB,
+		DOB,
+		KRZ,
+		KNL,
+		LOV,
+		MON,
+		PAZ,
+		PER,
+		PVN,
+		PDV,
+		RAZ,
+		RSE,
+		SLS,
+		SLV,
+		SML,
+		SFO,
+		SOF,
+		SZR,
+		TGV,
+		HKV,
+		SHU,
+		JAM
+	}
+}
