fc8c28b2e3a9b69a995ed7058cf957ae2f96ba34
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2021-06-19 11:20:52 +0300

    Add files via upload

---
+++ Coronavirus Web Scaper/Mongo.cs
@@ -0,0 +1,31 @@
+﻿using Coronavirus_Web_Scaper.Controllers.Mapping;
+using Coronavirus_Web_Scaper.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Configuration;
+
+namespace Coronavirus_Web_Scaper
+{
+	public static class Mongo
+	{
+		private static Rootobject CacheRootobject { get; set; }
+
+		public static IMongoCollection<Rootobject> GetCollection()
+		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+				.AddJsonFile("appsettings.json").Build();
+			var section = config.GetSection("ConnectionStrings");
+			var client = new MongoClient(section.Value);
+			return client.GetDatabase("Corona").GetCollection<Rootobject>("Corona");
+		}
+
+		public static Rootobject GetRootobject(IMongoCollection<Rootobject> collection)
+		{
+			var filter = Builders<Rootobject>.Filter.Eq("Country", "BG");
+			return collection.Find(filter).FirstOrDefault();
+		}
+    }
+}
---
+++ Coronavirus Web Scaper/Program.cs
@@ -0,0 +1,42 @@
+﻿using Coronavirus_Web_Scaper.Controllers.Mapping;
+using Coronavirus_Web_Scaper.Models;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Coronavirus_Web_Scaper
+{
+    public class Program
+    {
+        private static IMongoCollection<Rootobject> dbcollection = Mongo.GetCollection();
+        /// <summary>
+        /// Creting timer to know wnen to update the information in the database.
+        /// </summary>
+        //static Timer timer = new Timer(86400000);
+        static Timer timer = new Timer(10000);
+        /// <summary>
+        /// In the main method I check if there is a record in the database. If there is not I create it. 
+        /// After that I start the timer with atached event on it to update the information in the database.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
